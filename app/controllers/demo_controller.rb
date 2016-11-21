09e5ba4864bb41b11959698f7f3cc04dfb4947e8
jverkerk <fushti@gmail.com>
2016-11-21 15:26:17 +1000

    links

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -2,6 +2,18 @@ class DemoController < ApplicationController
   
   layout false
   
-  def index
-  end
+   def index
+   	render('index')
+   end 
+   
+   def hello
+   	@array = [1,2,3,4,5,6]
+   	@id = params['id']
+   	@page = params[:page]
+   	render('hello')
+   end
+   
+   def other_hello
+   	redirect_to(:action => 'hello')
+   end
 end
