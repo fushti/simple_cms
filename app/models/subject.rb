e6f4c731a0bb23fe29601bbf155e3c6a6063f615
jverkerk <fushti@gmail.com>
2016-11-23 14:17:12 +1000

    query stuff

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,2 +1,8 @@
 class Subject < ApplicationRecord
+	
+	scope :visible, -> { where(:visible => true) }
+	scope :invisible, -> { where(:visible => false) }
+	scope :sorted, -> { order("position ASC") }
+	scope :newest_first, -> { order("created_at DESC") }
+	scope :search, lambda {|q| where(["name LIKE ?", "%#{q}%"])}
 end
