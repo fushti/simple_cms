362c58fc7b498332248bd4659ee5a975f3b2a069
jverkerk <fushti@gmail.com>
2016-11-29 09:26:04 +1000

    pages and sections

--- app/models/page.rb
+++ app/models/page.rb
@@ -2,5 +2,10 @@ class Page < ApplicationRecord
 	
 	belongs_to :subject
 	has_and_belongs_to_many :admin_users
+
+	scope :visible, -> { where(:visible => true) }
+	scope :invisible, -> { where(:visible => false) }
+	scope :sorted, -> { order("position ASC") }
+	scope :newest_first, -> { order("created_at DESC") }
 	
 end
