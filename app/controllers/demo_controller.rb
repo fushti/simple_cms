e67f961f72739a9794a7928a204aca665a69843b
jverkerk <fushti@gmail.com>
2016-11-29 17:14:06 +1000

    sanatize helpers

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -2,18 +2,22 @@ class DemoController < ApplicationController
   
   layout false
   
-   def index
-   	render('index')
-   end 
-   
-   def hello
-   	@array = [1,2,3,4,5,6]
-   	@id = params['id']
-   	@page = params[:page]
-   	render('hello')
-   end
-   
-   def other_hello
-   	redirect_to(:action => 'hello')
-   end
+  def index
+    render('index')
+  end
+
+  def hello
+    @array = [1,2,3,4,5,6]
+    @id = params['id']
+    @page = params[:page]
+    render('hello')
+  end
+
+  def other_hello
+    redirect_to(:action => 'hello')
+  end
+
+  def escape_output
+
+  end
 end
