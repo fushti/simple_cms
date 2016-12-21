7b3bd6f5cdc74a6fdad13a11e6247214af689a8e
jverkerk <fushti@gmail.com>
2016-12-21 13:30:32 +1000

    validations

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -7,4 +7,6 @@ class Subject < ApplicationRecord
 	scope :sorted, -> { order("position ASC") }
 	scope :newest_first, -> { order("created_at DESC") }
 	scope :search, lambda {|q| where(["name LIKE ?", "%#{q}%"])}
+
+	validates_presence_of :name
 end
