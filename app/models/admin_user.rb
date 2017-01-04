506dc0d0317859e4229438ac8edccd686db1cd00
jverkerk <fushti@gmail.com>
2017-01-04 14:05:51 +1000

    custom validations

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -6,7 +6,9 @@ class AdminUser < ApplicationRecord
 	has_many :sections, :through => :section_edits
 
 	EMAIL_REGEX = /\A[a-z0-9._%+-]/i
+	FORBIDDEN_USERNAMES = ['littlebopeep','humptydumpty','marymary']
 
+	# long form validations
 	# validates_presence_of :first_name
 	# validates_length_of :first_name, :maximum => 25
 	# validates_presence_of :last_name
@@ -19,5 +21,27 @@ class AdminUser < ApplicationRecord
 	# validates_length_of :email, :maximum => 100
 	# validates_format_of :email, :with => EMAIL_REGEX
 	# validates_confirmation_of :email
+
+	# better validations
+	validates :first_name, :presence => true, :length => { :maximum => 25 }
+	validates :last_name, :presence => true, :length => {:maximum => 50}
+	validates :username, :length => {:within => 8..25}, :uniqueness => true
+	validates :email, :presence => true, :length => {:maximum => 100}, :format => EMAIL_REGEX, :confirmation => true
+	# Custom validations
+	validate :username_is_allowed
+	validate :no_new_users_on_wednesday, :on => :create
 	
+	private
+	
+	def username_is_allowed
+		if FORBIDDEN_USERNAMES.include?(username)
+			errors.add(:username, "has been restricted from use.")
+		end
+	end
+	
+	def no_new_users_on_wednesday
+		if Time.now.wday == 3
+			errors.add(:base, "No new users on Wednesdays.")
+		end
+	end
 end
