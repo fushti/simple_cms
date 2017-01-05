64f198e8076b7b22ec7b8f5bf060b15d81491bd0
jverkerk <fushti@gmail.com>
2017-01-06 09:57:23 +1000

    authentication

--- app/controllers/access_controller.rb
+++ app/controllers/access_controller.rb
@@ -1,7 +1,21 @@
 class AccessController < ApplicationController
+	
+	layout 'admin'
+	
   def menu
+  	# display text and links
   end
 
   def login
+  	# display login form
   end
+  
+  def attempt_login
+  	
+  end
+  
+  def logout
+  	
+  end
+  
 end
